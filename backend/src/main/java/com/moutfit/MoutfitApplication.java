e66dbb15515ce86507e1eda4e0e81d2904e0b2f1
murilofsouzaa <onemurilo@gmail.com>
2025-11-09 11:16:23 -0300

    basic structure react + springboot

---
+++ backend/src/main/java/com/moutfit/MoutfitApplication.java
@@ -0,0 +1,13 @@
+package com.moutfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoutfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoutfitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/moutfit/MoutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moutfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoutfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
