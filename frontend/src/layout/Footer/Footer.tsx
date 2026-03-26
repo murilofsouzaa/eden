5912bee53ad21a8485eb00f78fa9c4d753171285
murilofsouzaa <onemurilo@gmail.com>
2026-03-26 14:23:57 -0300

    fix: adding copyright text and aligning it in the container

--- frontend/src/layout/Footer/Footer.tsx
+++ frontend/src/layout/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer(){
     return (
         <div className="bg-black">
             <div className="flex flex-col gap-10 px-6 py-10 lg:flex-row lg:justify-center lg:items-center">
-                <div className="flex flex-col gap-10 lg:flex-row lg:w-full lg:h-full lg:gap-20 lg:ml-10">
+                <div className="flex gap-10 lg:flex-row lg:w-full lg:h-full lg:gap-20 lg:ml-10">
                     <div>
                         <h2 className="footer-h2">Coisas Chatas</h2>
                         <ul className="footer-info">
@@ -27,7 +27,9 @@ export function Footer(){
                         </ul>
                     </div>
                 </div>
-                    <div className="flex flex-col items-start gap-6 lg:items-end lg:w-auto lg:m-20">
+
+
+                <div className="flex flex-col items-start gap-6 lg:items-start lg:w-auto lg:m-20">
                     <div>
                         <h2 className="footer-h2">Atendimento</h2>
                         <ul className="footer-info">
@@ -37,7 +39,10 @@ export function Footer(){
                         </ul>
                     </div>
                     <div>
-                        <h2 className="font-bold mt-20 text-white">Desenvolvido por Murilo Freitas de Souza</h2>
+                        <p className="text-white">Este projeto utiliza imagens de terceiros, podendo estar protegidas por direitos autorais. Não reivindico qualquer direito sobre elas e, caso haja solicitação, removo imediatamente.</p>
+                    </div>
+                    <div>
+                        <h2 className="font-bold text-white">Desenvolvido por Murilo Freitas de Souza</h2>
                         <ul className="flex gap-4 mt-2">
                             <li><a href="https://github.com/murilofsouzaa" target="on_blank"><img src={githubIconWhite} alt="email-icon" className="footer-icon"/></a></li>
                             <li><a href="https://www.instagram.com/murilofsouza_/" target="on_blank"><img src={instagramIconWhite} alt="instagram-icon" className="footer-icon"/></a></li>
